f69c64fff4b19e1493f19c703095ad1fc2427732
farisalkhuzai <alkhuzaifaris@gmail.com>
2022-02-20 14:53:14 +0300

    functionBuildKey

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber){
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+  void buildKey(){
+    Expanded(
+      child: TextButton(onPressed: (){
+        playSound(1);
+      },
+        style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.red)),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(),
+              buildKey(),
+              buildKey(),
+              buildKey(),
+              buildKey(),
+              buildKey(),
+              buildKey(),
+            ],
+          ),
         ),
       ),
     );
